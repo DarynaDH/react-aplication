6e2d5b387eb29e35961af1a49b5b4a1984ad57bf
DrynaDH <darynahrytsay26@gmail.com>
2022-10-22 19:13:19 +0300

    react

---
+++ README.old.md
@@ -0,0 +1,1 @@
+dgherfjhfgndsdvbn xc
---
+++ src/components/component-1/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Component1 () {
+    return (
+        <diV>
+             <p>gh jcvklbhjmkbvhpgofvdxtastgvhujb dfrcgvhyuhgrfedwugt54vu67br rgedtgx5cvhyu6
+             </p>
+        </diV>
+    )
+
+
+
+}
+
+export default Component1;
